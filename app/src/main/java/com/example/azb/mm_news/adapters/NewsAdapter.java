c9007e31fbc2e791c01e929eb7e3adc859a94bc6
aungzanbaw <aungzanbaw@gmail.com>
2017-12-03 15:04:37 +0630

    onCreateViewHolder on Adapter and ItemView

--- app/src/main/java/com/example/azb/mm_news/adapters/NewsAdapter.java
+++ app/src/main/java/com/example/azb/mm_news/adapters/NewsAdapter.java
@@ -1,7 +1,12 @@
 package com.example.azb.mm_news.adapters;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import com.example.azb.mm_news.R;
+import com.example.azb.mm_news.viewholders.ItemNewsViewHolder;
 
 /**
  * Created by azb on 12/3/17.
@@ -10,7 +15,11 @@ import android.view.ViewGroup;
 public class NewsAdapter extends RecyclerView.Adapter {
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View newsItemView = inflater.inflate(R.layout.item_news, parent, false);
+        ItemNewsViewHolder itemNewsViewHolder = new ItemNewsViewHolder(newsItemView);
+        return itemNewsViewHolder;
     }
 
     @Override
@@ -20,6 +29,6 @@ public class NewsAdapter extends RecyclerView.Adapter {
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 16;
     }
 }
