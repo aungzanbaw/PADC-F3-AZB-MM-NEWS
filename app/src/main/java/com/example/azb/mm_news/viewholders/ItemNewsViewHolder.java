684e199acffac1039f1a913cb071c04c08fd0ab5
aungzanbaw <aungzanbaw@gmail.com>
2017-12-03 14:33:39 +0630

    Add ItemNews java class

---
+++ app/src/main/java/com/example/azb/mm_news/viewholders/ItemNewsViewHolder.java
@@ -0,0 +1,14 @@
+package com.example.azb.mm_news.viewholders;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by azb on 12/3/17.
+ */
+
+public class ItemNewsViewHolder extends RecyclerView.ViewHolder {
+    public ItemNewsViewHolder(View itemView) {
+        super(itemView);
+    }
+}
